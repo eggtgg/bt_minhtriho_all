1ce72133f4df01c8c0169a8fb2632ad159421240
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-05-21 10:46:01 +0700

    commit - bt moi

--- R/thuc_hanh_18_5_21/chuong7boxplot.R
+++ R/thuc_hanh_18_5_21/chuong7boxplot.R
@@ -17,13 +17,21 @@ boxplot(NegPerChick ~ Nest,
         axes=FALSE,
         ylim=c(-3,8.5),
         data = Owls)
-text(x=1:27, y=-2,
-     labels= levels(Owls$Nest),
-     cex=0.75, srt=65)
+unique(Owls$Nest)
+axis(2, at=c(0,2,4,6,8))
+text(x=1:27, y=-2, labels = unique(Owls$Nest),
+     cex=1, srt=65)
 # 7.3 Boxplot sử dụng cho dữ liệu sinh vật biển RIKZ2.txt
 # vẽ viểu đồ boxplot giữa
 # Richness ~ Beach
 
 RIKZ = read.table('RIKZ2.txt', header =TRUE)
 RIKZ
-boxplot(Richness ~ Beach, data = RIKZ)
+boxplot(Richness ~ Beach, data = RIKZ,
+        col ='grey', xlab="Beach",
+        ylab = 'Richness')
+
+
+Veg = read.table('Vegetation2.txt', header = TRUE)
+boxplot(R~Transect, data = Veg,
+        ylab = 'Richness')
