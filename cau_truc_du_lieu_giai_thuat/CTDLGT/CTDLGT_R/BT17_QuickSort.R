74e4b85143dc83a2a70bf9ae02607230f0288ece
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 10:38:40 +0700

    Update BT17_QuickSort.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT17_QuickSort.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT17_QuickSort.R
@@ -2,10 +2,18 @@
 
 vec = c(1,-2,3,6,2,1,9)
 
+# Ý tưởng:
+# Bước 1: Lấy phần tử chốt là phần tử ở cuối danh sách.
+# Bước 2: Chia mảng theo phần tử chốt.
+# Bước 3: Sử dụng sắp xếp nhanh một cách đệ qui với mảng con bên trái.
+# Bước 4: Sử dụng sắp xếp nhanh một cách đệ qui với mảng con bên phải.
+
+# Tạo hàm: truyền vào arr: list cần sắp xếp
 quickSort <- function(arr) {
   mid <- sample(arr, 1)
   left <- c()
   right <- c()
+  #Chia mảng theo phần tử chốt.
   lapply(arr[arr != mid], function(d) {
     if (d < mid) {
       left <<- c(left, d)
@@ -14,11 +22,11 @@ quickSort <- function(arr) {
       right <<- c(right, d)
     }
   })
-  
+  # đệ qui với mảng con bên trái.
   if (length(left) > 1) {
     left <- quickSort(left)
   }
-  
+  # đệ qui với mảng con bên phải.
   if (length(right) > 1) {
     right <- quickSort(right)
   }
