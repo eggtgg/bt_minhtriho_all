672f73938d5524eb8e08022364991c357d447316
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 09:35:31 +0700

    Update BT03_GiaiThua.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT03_GiaiThua.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT03_GiaiThua.R
@@ -1,9 +1,12 @@
 #Giai Thua
-
+# Tạo hàm nhận tham số là n
 giai_thua <- function(n){
+  # nếu n = 0 thì kết quả trả về là 1
   if(n == 0){
     return(1)
   } else {
+    # ngược lại trả về (n* hàm vừa khỏi tạo với tham số n-1)
+    # cho đến khi kết thúc bài toán (n=0)
     return(n*giai_thua(n-1))
   }
 }
