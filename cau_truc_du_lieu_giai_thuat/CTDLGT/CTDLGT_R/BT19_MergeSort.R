3ef4fe550f6ce9765e7effdf76e3a499b7a2f4cf
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 10:58:12 +0700

    Update BT19_MergeSort.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT19_MergeSort.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT19_MergeSort.R
@@ -1,3 +1,12 @@
+# mergesort
+
+# ý tưởng:
+# 1. Tìm chỉ số nằm giữa mảng để chia mảng thành 2 nửa:
+# 2. Gọi đệ quy hàm mergeSort cho nửa đầu tiên:   
+# 3. Gọi đệ quy hàm mergeSort cho nửa thứ hai:
+# 4. Gộp 2 nửa mảng đã sắp xếp ở (2) và (3):
+
+# Hàm nối các mảng đã tách ra
 mmerge<-function(a,b) {
   r<-numeric(length(a)+length(b))
   ai<-1; bi<-1; j<-1;
@@ -12,10 +21,13 @@ mmerge<-function(a,b) {
   }
   r
 }
+# Hàm chia mảng để sắp xếp
 mmergesort<-function(A) {
   if(length(A)>1) {
     q <- ceiling(length(A)/2)
+    # đệ quy hàm mergeSort cho nửa đầu tiên
     a <- mmergesort(A[1:q])
+    # đệ quy hàm mergeSort cho nữa thứ hai
     b <- mmergesort(A[(q+1):length(A)])
     mmerge(a,b)
   } else {
