ad8c529f6739286cc66d2ce44db735e8e5f2f910
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 10:25:02 +0700

    Update BT16_SapXepNoiBot.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT16_SapXepNoiBot.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT16_SapXepNoiBot.R
@@ -1,9 +1,17 @@
 #sap xep noi bot
 
+# Ý tưởng:
+# lặp lại công việc đổi chỗ 2 số liên tiếp nhau nếu chúng đứng
+# sai thứ tự(số sau bé hơn số trước với trường hợp sắp xếp tăng dần)
+# cho đến khi dãy số được sắp xếp.
 
+# tạo một vector cần sắp xếp
 vec = c(1,-1,3,2,10,9)
+
+# tạo hàm với tham số x là một list cần sắp xếp
 bubble <- function(x){
   n<-length(x)
+  # so sánh từng 2 số một liền kề
   for(j in 1:(n-1)){
     for(i in 1:(n-j)){
       if(x[i]>x[i+1]){
