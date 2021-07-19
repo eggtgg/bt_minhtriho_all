3db5ecf85b2a28d2d97ddbd4ba40b43cdbbe47cc
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 09:49:39 +0700

    Update BT12_BT13_DoThiVoHuong_DoThiCoHuong.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT12_BT13_DoThiVoHuong_DoThiCoHuong.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT12_BT13_DoThiVoHuong_DoThiCoHuong.R
@@ -5,12 +5,15 @@ library(igraph)
 
 # Vo Huong
 help(graph)
-vo_huong <- make_graph( ~ A-B-C-D-A, E-A:B:C:D, directed = FALSE)
+
+# A nối B nối C nối D nối A
+# E nối với A,B,C,D
+vo_huong <- make_graph( ~ A-B-C-D-A, E-A:B:C:D)
 plot(vo_huong)
 
 
 #Co huong
 help(make_directed_graph)
-
+# nối theo từng cặp một  (1,2), (2,3)....
 a = make_graph(c(1, 2, 2, 3, 3, 4, 5, 6, 4,1, 2,4, 4,5, 6,1), directed = TRUE)
 plot(a)
