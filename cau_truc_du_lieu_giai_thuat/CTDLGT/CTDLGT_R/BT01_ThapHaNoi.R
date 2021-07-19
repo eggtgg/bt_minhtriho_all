2236a12ed53bbbd35ddbb016ed44e687edbd9746
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 09:15:56 +0700

    Update BT01_ThapHaNoi.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT01_ThapHaNoi.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT01_ThapHaNoi.R
@@ -1,9 +1,15 @@
+# Tạo hàm nhập vào 4 giá trị:
+#...n: số tầng của tháp
+#...cac toa thap
 thap_ha_noi <- function(n, toa1, toa2, toa3){
+  # Nếu n chỉ còn 1 thì chuyển tầng này vào tòa số 3
   if (n==1){
     print(toa1)
     print('|')
     print(toa3)
     print('')
+    # Ngược lại, tiếp tục gọi hàm thap_ha_noi
+    # cho đến khi giải được bài toán (n=1)
   } else {
       thap_ha_noi(n-1,toa1, toa3, toa2)
       print(toa1)
