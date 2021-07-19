8147061c140077b9e179574011f8cc4ee09fc5b2
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 09:40:13 +0700

    Update BT06_BT07_DanhSachLienKet.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT06_BT07_DanhSachLienKet.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT06_BT07_DanhSachLienKet.R
@@ -1,10 +1,10 @@
 # danh sach lien ket
-lst <- list() # creates an empty (length zero) list
-lst[[1]] <- 1 # automagically extends the lst
+lst <- list() # tạo một danh sách trống (độ dài bằng 0)
+lst[[1]] <- 1 # tự động mở rộng lst
 lst[[2]] <- 2 # ditto
 lst
-lst <- list(1, 2, 3, 4, 5)    # a list of 5 items
+lst <- list(1, 2, 3, 4, 5)    # danh sách 5 mục
 lst <- vector("list", 10000)  # 10000 NULLs
 lst[[1]] <- 1
-lst[[10000]] <- 10000  # lst now contains 1, NULL, ..., NULL, 10000
+lst[[10000]] <- 10000  # lst hiện chứa 1, NULL, ..., NULL, 10000
 lst
