65c49c644f8113ade029848c98d4ac18d9687a60
eggtgg <73218635+eggtgg@users.noreply.github.com>
2021-07-19 09:18:02 +0700

    Update BT02_UCLN.R

--- cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT02_UCLN.R
+++ cau_truc_du_lieu_giai_thuat/CTDLGT/CTDLGT_R/BT02_UCLN.R
@@ -1,6 +1,10 @@
+# tạo hàm nhập vào 2 tham số a và b nguyen
 ucln <- function(a, b){
+  # nếu b= 0 thì UCLN sẽ bằng đúng a
   if(b == 0){
     return(a)
+    # ngược lại thì sẽ gọi lại hàm ucln
+    # cho đến khi bài toán được giải quyết khi (b=0)
   } else {
     return(ucln(b, a%%b))
   }
